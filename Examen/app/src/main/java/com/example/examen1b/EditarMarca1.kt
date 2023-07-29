8579e70314b42b97848a605a35175edf9e567d55
Kevin López <kevin.lopez03@epn.edu.ec>
2023-07-29 00:08:49 -0500

    Deber 02

---
+++ Examen/app/src/main/java/com/example/examen1b/EditarMarca1.kt
@@ -0,0 +1,11 @@
+package com.example.examen1b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarMarca1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_marca1)
+    }
+}
